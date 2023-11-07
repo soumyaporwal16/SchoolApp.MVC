6a50e8439e3ee65cb4fa24cdf36fc22fdc707787
soumyaporwal16 <amit.porwal27@gmail.com>
2023-11-07 09:26:05 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
